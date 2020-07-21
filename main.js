843f221e5732942561b3b802d8e87c52ac5c27f1
sympathyzz <51390756+sympathyzz@users.noreply.github.com>
2020-07-21 19:53:30 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
-function createMultiplyTable(start, end) {
-  return null;
+function printMultiplyTableByRange (start, end) {
+    if(isValid(start,end)){
+       return printMultiplyTable(start,end);
+    }
+    return "null";
 }
 
-module.exports = {
-  createMultiplyTable,
-};
+function isValid(start,end){
+    if(start>end||start<1||start>1000||end<1||end>1000){
+        return false;
+    }else{
+        return true;
+    }
+}
+
+function printMultiplyTable(start,end){
+    var result='';
+    for(var i=start;i<=end;i++){
+        for(var j=start;j<=i;j++){
+           result+=j+'*'+i+'='+j*i+'  ';
+        }
+        result+='\n';
+    }
+    return result;
+}
+
+
+module.exports = printMultiplyTableByRange;
